173793e7674e07f3afbd2b88e7cfe84601d6b966
Edgar Avecilla <eavecilla@pop-os.localdomain>
2023-07-07 16:52:07 -0600

    Added control buttons

--- lib/presentation/screens/counter_screen.dart
+++ lib/presentation/screens/counter_screen.dart
@@ -13,36 +13,71 @@ class _CounterScreenState extends State<CounterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Counter Screen'),
-        actions: [
-          IconButton(
-              onPressed: () {
-                setState(() {
-                  clickCounter = 0;
-                });
-              },
-              icon: const Icon(Icons.refresh))
-        ],
-      ),
-      body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Text('$clickCounter',
-              style:
-                  const TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
-          Text(clickCounter == 1 ? 'Click' : 'Clicks',
-              style: const TextStyle(fontSize: 25))
-        ]),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            clickCounter++;
-          });
-        },
-        child: const Icon(Icons.plus_one),
-      ),
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Counter Screen'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    clickCounter = 0;
+                  });
+                },
+                icon: const Icon(Icons.refresh))
+          ],
+        ),
+        body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text('$clickCounter',
+                style: const TextStyle(
+                    fontSize: 160, fontWeight: FontWeight.w100)),
+            Text(clickCounter == 1 ? 'Click' : 'Clicks',
+                style: const TextStyle(fontSize: 25))
+          ]),
+        ),
+        floatingActionButton:
+            Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+          CustomButton(
+            Icons.refresh_rounded,
+            onPressed: () {
+              setState(() {
+                clickCounter = 0;
+              });
+            },
+          ),
+          const SizedBox(height: 15),
+          CustomButton(
+            Icons.plus_one,
+            onPressed: () {
+              setState(() {
+                clickCounter++;
+              });
+            },
+          ),
+          const SizedBox(height: 15),
+          CustomButton(
+            Icons.exposure_minus_1,
+            onPressed: () {
+              setState(() {
+                if (clickCounter > 0) clickCounter--;
+              });
+            },
+          )
+        ]));
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback? onPressed;
+
+  const CustomButton(this.icon, {super.key, this.onPressed});
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      shape: const StadiumBorder(),
+      onPressed: onPressed,
+      child: Icon(icon),
     );
   }
 }
